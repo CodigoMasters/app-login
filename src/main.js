5173a0bead7ef33eef742338f848b88e624ac19c
Victor De la cruz <codigosmasters@gmail.com>
2026-02-16 21:49:58 -0500

    feat: cambio de diseño en CSS

--- src/main.js
+++ src/main.js
@@ -1,26 +1,34 @@
 const app = document.querySelector("#app");
+
 let contador = 0;
 
-function render() {
-  app.innerHTML = `
-    <div style="text-align: center; margin-top: 50px;">
-      <h1>Contador moderno</h1>
-      <p style="font-size: 20px; font-weight: bold;">${contador}</p>
-      <button id="btn-sumar">Sumar +1</button>
-      <button id="btn-restar">Restar -1</button>
+app.innerHTML = `
+  <main class="counter">
+    <h1 class="counter__title">Contador moderno</h1>
+    <div class="counter__display">
+      <span id="valor" class="counter__value">${contador}</span>
     </div>
-  `;
-  const btnSumar = document.querySelector("#btn-sumar");
-  const btnRestar = document.querySelector("#btn-restar");
+    <div class="counter__actions">
+      <button id="btn-sumar" class="counter__btn counter__btn--add">Sumar +1</button>
+      <button id="btn-restar" class="counter__btn counter__btn--subtract">Restar -1</button>
+    </div>
+  </main>
+`;
 
-  btnSumar.addEventListener("click", () => {
-    contador++;
-    render();
-  });
-  btnRestar.addEventListener("click", () => {
-    contador--;
-    render();
-  });
-}
+const valor = document.querySelector("#valor");
+const btnSumar = document.querySelector("#btn-sumar");
+const btnRestar = document.querySelector("#btn-restar");
 
-render();
+btnSumar.addEventListener("click", () => {
+  contador++;
+  actualizar();
+});
+
+btnRestar.addEventListener("click", () => {
+  contador--;
+  actualizar();
+});
+
+function actualizar() {
+  valor.textContent = contador;
+};
